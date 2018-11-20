ad2c2befd6e2137475cf01d557309256ef33b317
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-11-20 22:51:51 +0800

    Websocket功能完善

--- src/main/java/com/bonaparte/config/WebSocketConfig.java
+++ src/main/java/com/bonaparte/config/WebSocketConfig.java
@@ -1,5 +1,9 @@
 package com.bonaparte.config;
 
+import com.bonaparte.bean.RoomFactory;
+import com.bonaparte.bean.STOMPConnectEventListener;
+import com.bonaparte.bean.SocketSessionRegistry;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -12,12 +16,26 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
 
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
-
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/queue","/topic");
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry config){
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/my-websocket").setAllowedOrigins("*").withSockJS();
+    }
 
+    @Bean
+    public SocketSessionRegistry SocketSessionRegistry(){
+        return new SocketSessionRegistry();
+    }
+    @Bean
+    public STOMPConnectEventListener STOMPConnectEventListener(){
+        return new STOMPConnectEventListener();
+    }
+    @Bean
+    public RoomFactory roomFactory(){
+        return new RoomFactory();
     }
 }
